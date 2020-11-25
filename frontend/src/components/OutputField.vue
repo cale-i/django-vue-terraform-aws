279c62b6ae59794d06f12d1ef97b0e184090fc36
cale-i <calei078029@gmail.com>
2020-11-26 02:11:34 +0900

    add get and delete method

--- frontend/src/components/OutputField.vue
+++ frontend/src/components/OutputField.vue
@@ -1,11 +1,65 @@
 <template>
     <div id="output-field">
-        output
-    </div>
+        <b-container fluid>
+
+            <b-button @click="reloadTable" variant="danger">reload</b-button>
+            <b-table 
+                sticky-header
+                striped
+                hover
+                :items="items"
+                :fields="fields"
+            >
+                <template #cell(edit)="row">
+                    <b-button size="sm" @click="deleteItem(row.item)">
+                        DELETE
+                    </b-button>
+                </template>
+            </b-table>
+        </b-container>
+
+   </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                items: [{
+                }],
+                fields: [
+                    {key: 'id', label:'ID',},
+                    {key: 'title', label:'Title'},
+                    {key: 'price', label:'Price'},
+                    {key: 'created_at', label:'date'},
+                    {key: 'edit', label:'Edit'},
+                    
+                ]
+            }
+        },
+        methods: {
+            reloadTable() {
+                this.$store.dispatch(
+                    'item/retlieve'
+                )
+                .then(response => {
+                    this.items = response.results
+                })
+            },
+            deleteItem(item) {
+                this.$store.dispatch(
+                    'item/delete',
+                    {item: item}
+                )
+                .then(() =>{
+                    this.reloadTable()
+                }
+                )
+            }
+        },
+        created() {
+            this.reloadTable()
+        }
     }
 </script>
 
